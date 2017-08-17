01453e33a1a27fc0d7ac9709ffdf5b78170907ef
Allen Barr <captainbowtie@users.noreply.github.com>
2017-08-17 15:19:13 -0500

    Added ODS to gitignore; pointed login to correct database

--- Tabulation-Web-Application/login.php
+++ Tabulation-Web-Application/login.php
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-$db_hostname = '192.168.56.101';
-$db_database = 'publications';
+$db_hostname = 'tabtesting';
+$db_database = 'tournaments';
 $db_username = 'test';
 $db_password = 'password';
 ?>
