e8411a87dd11d97a72006e6412b946ad2ba5cad5
Allen Barr <captainbowtie@users.noreply.github.com>
2017-08-19 14:42:08 -0500

    Customized header menu to user level
    Header now checks to see what type of user is logged in (coach, judge,
    tab, or student) and displays the links appropriate to the user role

--- Tabulation-Web-Application/header.php
+++ Tabulation-Web-Application/header.php
@@ -33,18 +33,43 @@ if (isset($_POST["email"]) && isset($_POST["pass"])){
     $hashedPass = hash('whirlpool',$pass);
     $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
     $query = "SELECT * FROM users WHERE email='$email' && password='$hashedPass'";
+    //TODO: Stuff if the password is incorrect
     $result = $connection->query($query);
-    $id = $result->fetch_assoc()['id'];
+    $result->data_seek(0);
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    $id = $row['id'];
+    $isCoach = $row['isCoach'];
+    $isJudge = $row['isJudge'];
+    $isTab = $row['isTab'];
     $_SESSION['id'] = $id;
+    $_SESSION['isCoach'] = $isCoach;
+    $_SESSION['isJudge'] = $isJudge;
+    $_SESSION['isTab'] = $isTab;
+    $result->close();
 }
 
+echo "<div>";
+if($_SESSION['isTab']){
+    echo '<a href="/tabroom.php">Tab Room</a>';
+    echo '<a href="/tabsummary.php">Tab Summary</a>';
+    echo '<a href="/tabcards.php">Tab Cards</a>';
+}
+if($_SESSION['isJudge']){
+    echo '<a href="/ballot.php">Ballot</a>';
+}
+if($_SESSION['isCoach']){
+    echo '<a href="/tabsummary.php">Tab Summary</a>';
+    echo '<a href="/tabcards.php">Tab Cards</a>';
+}
 echo <<<_END
-<div>
 <a href="/pairings.php">Round Pairings</a>
 <a href="/locations.php">Locations</a>
 <a href="/map.php">Map</a>
 <a href="/contact.php">Contact</a>
 _END;
+if(isset($_SESSION['id'])){
+    echo '<a href="/alerts.php">Alerts</a>';
+}
 
 if(isset($_SESSION['id'])){
     $id = $_SESSION['id'];
