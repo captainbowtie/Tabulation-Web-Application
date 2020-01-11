59d423f4a37a0d2bb90303fcf31e3d7a0b968fe3
captainbowtie <allen@allenbarr.com>
2020-01-11 02:26:51 +0000

    create classes

---
+++ Tabulation-Web-Application/objects/pairing.php
@@ -0,0 +1,35 @@
+<?php
+/* 
+ * Copyright (C) 2019 allen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Ballot{
+ 
+    // database connection and table name
+    private $db;
+    private $table_name = "ballots";
+ 
+    // object properties
+    public $id;
+    public $pairing;
+    public $plaintiffPD;
+ 
+    // constructor with $db as database connection
+    public function __construct($conn){
+        $this->db = $conn;
+    }
+}
+?>
