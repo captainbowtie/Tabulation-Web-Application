2cef69d5ae006070208bcc2dc5d63ccc4768ca12
Allen Barr <captainbowtie@users.noreply.github.com>
2020-10-23 11:57:12 -0500

    Updated jquery and bootstrap versions in judges and judgeBallot; Added isByeTeam column to teams table

--- Tabulation-Web-Application/judgeBallot.php
+++ Tabulation-Web-Application/judgeBallot.php
@@ -160,7 +160,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width = device-width, initial-scale = 1.0">
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
         <!--Latest compiled and minified CSS-->
@@ -171,7 +171,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
         <link rel="stylesheet" href="judgeBallot.css">
 
-        <title></title>
+        <title>Ballot</title>
     </head>
     <body>
         <div>
