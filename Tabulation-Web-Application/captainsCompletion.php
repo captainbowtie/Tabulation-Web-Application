e17ba6b6a0001830b1ad9302d496f174768c0c44
Allen Barr <captainbowtie@users.noreply.github.com>
2021-10-19 16:32:19 -0500

    Removed unnecessary code from captainsCompletion Code was copied/pasted from judges page but isn't necessary here

--- Tabulation-Web-Application/captainsCompletion.php
+++ Tabulation-Web-Application/captainsCompletion.php
@@ -53,18 +53,18 @@ if ($_SESSION["isAdmin"]) {
             $bodyHTML .= "<tr>\n";
             $bodyHTML .= "<td>\n";
             $bodyHTML .= $pairing["plaintiff"];
-            if($pairingData["pOpen"]==="N/A"){
+            if ($pairingData["pOpen"] === "N/A") {
                 $bodyHTML .= "(Not Completed)";
-            }else{
+            } else {
                 $bodyHTML .= "(Completed)";
                 $bodyHTML .= $pairing["pOpen"];
             }
             $bodyHTML .= "</td>\n";
             $bodyHTML .= "<td>\n";
             $bodyHTML .= $pairing["defense"];
-            if($pairingData["dOpen"]==="N/A"){
+            if ($pairingData["dOpen"] === "N/A") {
                 $bodyHTML .= "(Not Completed)";
-            }else{
+            } else {
                 $bodyHTML .= "(Completed)";
                 $bodyHTML .= $pairing["pOpen"];
             }
@@ -113,69 +113,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
         <title>Judges</title>
     </head>
     <body>
-<?php
-echo $header;
-echo $bodyHTML;
-?>
-
-        <!-- New Judge Modal -->
-        <div id="newJudgeModal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">New Judge</h4>
-                    </div>
-                    <div class="modal-body"><form>
-                            <label for="name">Name:</label>
-                            <input id="name" name="name"><br>
-                            <label for="category">Category:</label>
-                            <select id="category" name="category">
-                                <option value="1">1: Non-Coach Judges, Trial Attorneys, Litigators</option>
-                                <option value="2">2: Other Non-Coach Attorneys</option>
-                                <option value="3">3: Coaches, Law Students, and Other Non-Attorneys</option>
-                            </select><br>
-                            <label for="round1">Round 1:</label>
-                            <input type="checkbox" id="round1" name="round1"><br>
-                            <label for="round1">Round 2:</label>
-                            <input type="checkbox" id="round2" name="round2"><br>
-                            <label for="round1">Round 3:</label>
-                            <input type="checkbox" id="round3" name="round3"><br>
-                            <label for="round1">Round 4:</label>
-                            <input type="checkbox" id="round4" name="round4"><br>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" id="addJudge" class="btn btn-default" data-dismiss="modal">Add Judge</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Conflicts Modal -->
-        <div id="conflictsModal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Judge Conflicts</h4>
-                    </div>
-                    <div class="modal-body"><form>
-<?php
-echo $conflictsHTML;
-?>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <script src="judges.js"></script>
+        <?php
+        echo $header;
+        echo $bodyHTML;
+        ?>
     </body>
 </html>
