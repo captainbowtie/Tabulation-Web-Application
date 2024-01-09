a4de540cdfef8413f64290027322b5e15b41c8d3
Allen Barr <captainbowtie@users.noreply.github.com>
2024-01-09 10:09:53 -0600

    Added 2024 witnesses
    Changed captains form for 2024 witnesses

--- captains.php
+++ captains.php
@@ -92,148 +92,150 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width = device-width, initial-scale = 1.0">
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <!--Latest compiled and minified JavaScript-->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-        <!--Latest compiled and minified CSS-->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <!--Latest compiled and minified JavaScript-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!--Latest compiled and minified CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-        <link rel="stylesheet" href="captains.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-        <title>Captains</title>
-    </head>
-    <body>
-        <?php
-        echo $header;
-        ?>
-        <div id="selectDiv">
-            <select id="pairingSelect">
-                <?php echo $pairingOptionsHTML; ?>
-            </select>
-        </div>
-        <div id="sideSelectDiv">
-            <input type ="radio" name="side" value="plainitff" id="pRadio">
-            <label for="pRadio">Plaintiff</label><br>
-            <input type="radio" name="side" value="defense" id="dRadio">
-            <label for="dRadio">Defense</label><br>
-        </div>
-        <div id="inputDiv">
-            <label id="pOpenLabel" for="pOpen">Prosecution Open:</label>
-            <input class="pInput" id="pOpen">
-            <label id="dOpenLabel" for="dOpen">Defense Open:</label>
-            <input class="dInput" id="dOpen">
+    <link rel="stylesheet" href="captains.css">
 
-            <select class="pInput" id="wit1">
-                <option value="Felder">Felder</option>
-                <option value="Hubbard">Hubbard</option>
-                <option value="Savchenko">Savchenko</option>
-                <option value="Gelfand">Gelfand</option>
-                <option value="Navarra">Navarra</option>
-                <option value="Roy">Roy</option>
-                <option value="Shahid">Shahid</option>
-            </select>
-            <label id="pDx1Label" for="pDx1">Directing Attorney:</label>
-            <input class="pInput" id="pDx1">
-            <label id="pWDx1Label" for="pWDx1">Witness:</label>
-            <input class="pInput" id="pWDx1">
-            <label id="dCx1Label" for="dCx1">Crossing Attorney:</label>
-            <input class="dInput" id="dCx1">
+    <title>Captains</title>
+</head>
 
-            <select class="pInput" id="wit2">
-                <option value="Felder">Felder</option>
-                <option value="Hubbard">Hubbard</option>
-                <option value="Savchenko">Savchenko</option>
-                <option value="Gelfand">Gelfand</option>
-                <option value="Navarra">Navarra</option>
-                <option value="Roy">Roy</option>
-                <option value="Shahid">Shahid</option>
-            </select>
-            <label id="pDx2Label" for="pDx2">Directing Attorney:</label>
-            <input class="pInput" id="pDx2">
-            <label id="pWDx2Label" for="pWDx2">Witness:</label>
-            <input class="pInput" id="pWDx2">
-            <label id="dCx2Label" for="dCx2">Crossing Attorney:</label>
-            <input class="dInput" id="dCx2">
+<body>
+    <?php
+    echo $header;
+    ?>
+    <div id="selectDiv">
+        <select id="pairingSelect">
+            <?php echo $pairingOptionsHTML; ?>
+        </select>
+    </div>
+    <div id="sideSelectDiv">
+        <input type="radio" name="side" value="plainitff" id="pRadio">
+        <label for="pRadio">Plaintiff</label><br>
+        <input type="radio" name="side" value="defense" id="dRadio">
+        <label for="dRadio">Defense</label><br>
+    </div>
+    <div id="inputDiv">
+        <label id="pOpenLabel" for="pOpen">Prosecution Open:</label>
+        <input class="pInput" id="pOpen">
+        <label id="dOpenLabel" for="dOpen">Defense Open:</label>
+        <input class="dInput" id="dOpen">
 
-            <select class="pInput" id="wit3">
-                <option value="Felder">Felder</option>
-                <option value="Hubbard">Hubbard</option>
-                <option value="Savchenko">Savchenko</option>
-                <option value="Gelfand">Gelfand</option>
-                <option value="Navarra">Navarra</option>
-                <option value="Roy">Roy</option>
-                <option value="Shahid">Shahid</option>
-            </select>
-            <label id="pDx3Label" for="pDx3">Directing Attorney:</label>
-            <input class="pInput" id="pDx3">
-            <label id="pWDx3Label" for="pWDx3">Witness:</label>
-            <input class="pInput" id="pWDx3">
-            <label id="dCx3Label" for="dCx3">Crossing Attorney:</label>
-            <input class="dInput" id="dCx3">
+        <select class="pInput" id="wit1">
+            <option value="Burke">Burke</option>
+            <option value="Bahmani">Bahmani</option>
+            <option value="Nguyen">Nguyen</option>
+            <option value="Nova">Nova</option>
+            <option value="Sands">Sands</option>
+            <option value="Cosmos">Cosmos</option>
+            <option value="Cage">Cage</option>
+        </select>
+        <label id="pDx1Label" for="pDx1">Directing Attorney:</label>
+        <input class="pInput" id="pDx1">
+        <label id="pWDx1Label" for="pWDx1">Witness:</label>
+        <input class="pInput" id="pWDx1">
+        <label id="dCx1Label" for="dCx1">Crossing Attorney:</label>
+        <input class="dInput" id="dCx1">
 
-            <select class="dInput" id="wit4">
-                <option value="Koller">Koller</option>
-                <option value="Heisman">Heisman</option>
-                <option value="Moore">Moore</option>
-                <option value="Gelfand">Gelfand</option>
-                <option value="Navarra">Navarra</option>
-                <option value="Roy">Roy</option>
-                <option value="Shahid">Shahid</option>
-            </select>
-            <label id="dDx1Label" for="dDx1">Directing Attorney:</label>
-            <input class="dInput" id="dDx1">
-            <label id="dWDx1Label" for="dWDx1">Witness:</label>
-            <input class="dInput" id="dWDx1">
-            <label id="pCx1Label" for="pCx1">Crossing Attorney:</label>
-            <input class="pInput" id="pCx1">
+        <select class="pInput" id="wit2">
+            <option value="Burke">Burke</option>
+            <option value="Bahmani">Bahmani</option>
+            <option value="Nguyen">Nguyen</option>
+            <option value="Nova">Nova</option>
+            <option value="Sands">Sands</option>
+            <option value="Cosmos">Cosmos</option>
+            <option value="Cage">Cage</option>
+        </select>
+        <label id="pDx2Label" for="pDx2">Directing Attorney:</label>
+        <input class="pInput" id="pDx2">
+        <label id="pWDx2Label" for="pWDx2">Witness:</label>
+        <input class="pInput" id="pWDx2">
+        <label id="dCx2Label" for="dCx2">Crossing Attorney:</label>
+        <input class="dInput" id="dCx2">
 
-            <select class="dInput" id="wit5">
-                <option value="Koller">Koller</option>
-                <option value="Heisman">Heisman</option>
-                <option value="Moore">Moore</option>
-                <option value="Gelfand">Gelfand</option>
-                <option value="Navarra">Navarra</option>
-                <option value="Roy">Roy</option>
-                <option value="Shahid">Shahid</option>
-            </select>
-            <label id="dDx2Label" for="dDx2">Directing Attorney:</label>
-            <input class="dInput" id="dDx2">
-            <label id="dWDx2Label" for="dWDx2">Witness:</label>
-            <input class="dInput" id="dWDx2">
-            <label id="pCx2Label" for="pCx2">Crossing Attorney:</label>
-            <input class="pInput" id="pCx2">
+        <select class="pInput" id="wit3">
+            <option value="Burke">Burke</option>
+            <option value="Bahmani">Bahmani</option>
+            <option value="Nguyen">Nguyen</option>
+            <option value="Nova">Nova</option>
+            <option value="Sands">Sands</option>
+            <option value="Cosmos">Cosmos</option>
+            <option value="Cage">Cage</option>
+        </select>
+        <label id="pDx3Label" for="pDx3">Directing Attorney:</label>
+        <input class="pInput" id="pDx3">
+        <label id="pWDx3Label" for="pWDx3">Witness:</label>
+        <input class="pInput" id="pWDx3">
+        <label id="dCx3Label" for="dCx3">Crossing Attorney:</label>
+        <input class="dInput" id="dCx3">
 
-            <select class="dInput" id="wit6">
-                <option value="Koller">Koller</option>
-                <option value="Heisman">Heisman</option>
-                <option value="Moore">Moore</option>
-                <option value="Gelfand">Gelfand</option>
-                <option value="Navarra">Navarra</option>
-                <option value="Roy">Roy</option>
-                <option value="Shahid">Shahid</option>
-            </select>
-            <label id="dDx3Label" for="dDx3">Directing Attorney:</label>
-            <input class="dInput" id="dDx3">
-            <label id="dWDx3Label" for="dWDx3">Witness:</label>
-            <input class="dInput" id="dWDx3">
-            <label id="pCx3Label" for="pCx3">Crossing Attorney:</label>
-            <input class="pInput" id="pCx3">
+        <select class="dInput" id="wit4">
+            <option value="Cameron">Cameron</option>
+            <option value="Porta">Porta</option>
+            <option value="Ebi">Ebi</option>
+            <option value="Orlov">Orlov</option>
+            <option value="Cosmos">Cosmos</option>
+            <option value="Cage">Cage</option>
+        </select>
+        <label id="dDx1Label" for="dDx1">Directing Attorney:</label>
+        <input class="dInput" id="dDx1">
+        <label id="dWDx1Label" for="dWDx1">Witness:</label>
+        <input class="dInput" id="dWDx1">
+        <label id="pCx1Label" for="pCx1">Crossing Attorney:</label>
+        <input class="pInput" id="pCx1">
 
-            <label id="pCloseLabel" for="pClose">Prosecution Close:</label>
-            <input class="pInput" id="pClose">
-            <label id="dCloseLabel" for="dClose">Defense Close:</label>
-            <input class="dInput" id="dClose">
-        </div>
-        <button id="submit">Submit</button>
-        <script>const round = <?php echo $maxRound; ?></script>
-        <script src="captains.js"></script>
-    </body>
+        <select class="dInput" id="wit5">
+            <option value="Cameron">Cameron</option>
+            <option value="Porta">Porta</option>
+            <option value="Ebi">Ebi</option>
+            <option value="Orlov">Orlov</option>
+            <option value="Cosmos">Cosmos</option>
+            <option value="Cage">Cage</option>
+        </select>
+        <label id="dDx2Label" for="dDx2">Directing Attorney:</label>
+        <input class="dInput" id="dDx2">
+        <label id="dWDx2Label" for="dWDx2">Witness:</label>
+        <input class="dInput" id="dWDx2">
+        <label id="pCx2Label" for="pCx2">Crossing Attorney:</label>
+        <input class="pInput" id="pCx2">
+
+        <select class="dInput" id="wit6">
+            <option value="Cameron">Cameron</option>
+            <option value="Porta">Porta</option>
+            <option value="Ebi">Ebi</option>
+            <option value="Orlov">Orlov</option>
+            <option value="Cosmos">Cosmos</option>
+            <option value="Cage">Cage</option>
+        </select>
+        <label id="dDx3Label" for="dDx3">Directing Attorney:</label>
+        <input class="dInput" id="dDx3">
+        <label id="dWDx3Label" for="dWDx3">Witness:</label>
+        <input class="dInput" id="dWDx3">
+        <label id="pCx3Label" for="pCx3">Crossing Attorney:</label>
+        <input class="pInput" id="pCx3">
+
+        <label id="pCloseLabel" for="pClose">Prosecution Close:</label>
+        <input class="pInput" id="pClose">
+        <label id="dCloseLabel" for="dClose">Defense Close:</label>
+        <input class="dInput" id="dClose">
+    </div>
+    <button id="submit">Submit</button>
+    <script>
+        const round = <?php echo $maxRound; ?>
+    </script>
+    <script src="captains.js"></script>
+</body>
+
 </html>
